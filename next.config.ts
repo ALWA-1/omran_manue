22e56ae5495347f013fb5c48f08ea044ef340603
ALWA-1 <169085545+ALWA-1@users.noreply.github.com>
2026-04-15 16:02:51 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+// إعدادات الـ PWA
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // إيقاف الكاش في بيئة التطوير لتسهيل التعديل
+  register: true,
+  sw: "sw.js",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+// تغليف الإعدادات الأصلية بإضافة الـ PWA
+export default withPWA(nextConfig);
